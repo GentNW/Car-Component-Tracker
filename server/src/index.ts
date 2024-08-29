12d83be16c4b97b9ed84c80fe688a93ec3dd0042
Zeyad Abdelrahman <89643048+GentNW@users.noreply.github.com>
2024-08-29 13:57:39 +0300

    Commiting changes before deleting frontend

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,19 @@
-import express from 'express';
+import express from 'express'
+import UserRoutes from './Routes/UserRoutes'
+import ComponentRoutes from './Routes/ComponentRoutes'
+import CarRoutes from './Routes/CarRoutes'
+
 
 const app = express();
+
+app.use(express.json())
+
+app.use('/api',UserRoutes)
+app.use('/api',CarRoutes)
+app.use('/api',ComponentRoutes)
+
+
+
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
