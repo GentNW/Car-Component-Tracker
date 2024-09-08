b4c55ee295fe2160acffc2cab227a47cea295ad4
Zeyad Abdelrahman <89643048+GentNW@users.noreply.github.com>
2024-09-08 14:32:39 +0300

    Added Edit Component Form(git didn't commit the change)

--- frontend/src/Features/Component/EditComponentForm.tsx
+++ frontend/src/Features/Component/EditComponentForm.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+const EditComponentForm = () =>{
+    let compnameplaceholder: string ="yes"
 
-const EditComponentForm = () => {
-  return (
-    <div>EditComponentForm</div>
-  )
+    const content = (
+    <>
+        <form className='CompForm'>
+            <div className='CompContainer'>
+                <label className="InputLabel">Component Name</label>
+                <label className='InputLabel'>{compnameplaceholder}</label>  
+                <br/>
+                <label className="InputLabel">Change date</label>
+                <input type="date" id="ChangeDate" name="ChangeDate"/>
+                <br/>
+                <label className="InputLabel">Mileage on change</label>
+                <input type="number" min="1" id="MileChange" name="MileChange"/>
+                <br/>
+                
+                <input type="submit" value="Submit"/>
+            </div>
+        </form>
+    </>
+    )
+    return content
 }
 
 export default EditComponentForm
