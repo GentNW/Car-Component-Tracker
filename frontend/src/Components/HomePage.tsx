7e321db524086d5b2bd2113e44643e2152f38e18
Zeyad Abdelrahman <89643048+GentNW@users.noreply.github.com>
2024-09-16 14:27:16 +0300

    Added Sign up form

--- frontend/src/Components/HomePage.tsx
+++ frontend/src/Components/HomePage.tsx
@@ -1,17 +1,30 @@
-import { Link } from "react-router-dom";
 const Homepage = () => {
     
+    function blur(){
+        const content = document.querySelector(".Main-Header")!
+        content.classList.toggle("blur")
+    }
+    function displayform (form: HTMLElement,otherform: HTMLElement){   
+
+        if((form.style.display === 'none' || form.style.display === '') && (otherform?.style.display==='none' || otherform?.style.display==='') ){
+            form.style.display = 'block'
+            blur()
+        }else if(form.style.display === 'block'){
+            form.style.display = 'none'
+            blur()
+        }
+    }
 
     function OnLoginClick(){
         const form = document.getElementById("Login-Form")!
-        const content = document.querySelector(".Main-Header")!
-        if(form.style.display === 'none' || form.style.display === ''){
-            form.style.display = 'block'
-            content.classList.toggle("blur")
-        }else{
-            form.style.display = 'none'
-            content.classList.toggle("blur")
-        }
+        const otherform = document.getElementById('Signup-Form')!
+        displayform(form,otherform)
+    }
+
+    function OnSignupClick(){
+        const form = document.getElementById("Signup-Form")!
+        const otherform = document.getElementById("Login-Form")!
+        displayform(form,otherform)
     }
 
     const content = (
@@ -25,13 +38,13 @@ const Homepage = () => {
                         <h1 className='Main-title'>Welcome to Car Doctor!</h1>
                         <nav className='Main-buttons'>
                         <button className='Main-button' onClick={OnLoginClick}>Login</button>
-                        <Link to="/register"><button className='Main-button'>Sign up</button></Link>
+                        <button className='Main-button' onClick={OnSignupClick}>Sign up</button>
                         </nav>
                 </div>
                 
-                <form id='Login-Form' className='Login-Form'>
+                <form id='Login-Form' className='Main-Form'>
                     <img className="Exit-Button" src="https://cdn-icons-png.flaticon.com/512/75/75519.png" alt="exit button" onClick={OnLoginClick}></img>    
-                    <div className='Login-Container'>
+                    <div className='Main-Container'>
 
                         <label className="InputLabel">Username</label>
                         <input type="text" id="username" name="username"/>
@@ -40,7 +53,22 @@ const Homepage = () => {
                         <input type="password" id="username" name="username"/>
                         <br/>
                         
-                        <input type="submit" value="Submit"/>
+                        <input type="submit" value="Login"/>
+                    </div>
+                </form>
+
+                <form id='Signup-Form' className='Main-Form'>
+                    <img className="Exit-Button" src="https://cdn-icons-png.flaticon.com/512/75/75519.png" alt="exit button" onClick={OnSignupClick}></img>    
+                    <div className='Main-Container'>
+                        
+                        <label className="InputLabel">Username</label>
+                        <input type="text" id="username" name="username"/>
+                        <br/>
+                        <label className="InputLabel">Password</label>
+                        <input type="password" id="password" name="password"/>
+                        <br/>
+                        
+                        <input type="submit" value="Sign up"/>
                     </div>
                 </form>
                 
