2e912f6b24dff90e3b4dc02c7fa594c15d75d12c
Zeyad Abdelrahman <89643048+GentNW@users.noreply.github.com>
2024-09-16 12:46:46 +0300

    added vfx and login form

--- frontend/src/Components/HomePage.tsx
+++ frontend/src/Components/HomePage.tsx
@@ -1,20 +1,48 @@
 import { Link } from "react-router-dom";
 const Homepage = () => {
     
+
+    function OnLoginClick(){
+        const form = document.getElementById("Login-Form")!
+        const content = document.querySelector(".Main-Header")!
+        if(form.style.display === 'none' || form.style.display === ''){
+            form.style.display = 'block'
+            content.classList.toggle("blur")
+        }else{
+            form.style.display = 'none'
+            content.classList.toggle("blur")
+        }
+    }
+
     const content = (
         <section className=''>
             {/*<header>
                 <p className=''>Your station for self expression!</p>
             </header>*/}
 
-            <main className='Main-content'>
-                <div className=''>
+            <main id='Main-Content' className='Main-Content'>
+                <div id='Main-Header' className='Main-Header'>
                         <h1 className='Main-title'>Welcome to Car Doctor!</h1>
                         <nav className='Main-buttons'>
-                            <span className='Main-button'><Link to="/login">Login</Link></span>
-                            <span className='Main-button'><Link to="/register">Sign up</Link></span>
+                        <button className='Main-button' onClick={OnLoginClick}>Login</button>
+                        <Link to="/register"><button className='Main-button'>Sign up</button></Link>
                         </nav>
                 </div>
+                
+                <form id='Login-Form' className='Login-Form'>
+                    <img className="Exit-Button" src="https://cdn-icons-png.flaticon.com/512/75/75519.png" alt="exit button" onClick={OnLoginClick}></img>    
+                    <div className='Login-Container'>
+
+                        <label className="InputLabel">Username</label>
+                        <input type="text" id="username" name="username"/>
+                        <br/>
+                        <label className="InputLabel">Password</label>
+                        <input type="password" id="username" name="username"/>
+                        <br/>
+                        
+                        <input type="submit" value="Submit"/>
+                    </div>
+                </form>
                 
             </main>
             {/*<footer>
