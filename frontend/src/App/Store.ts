7a9a0865cf98e5f04edf3a76c95353604621700a
Zeyad Abdelrahman <89643048+GentNW@users.noreply.github.com>
2024-09-23 16:00:29 +0300

    Added car & component reducers

--- frontend/src/App/Store.ts
+++ frontend/src/App/Store.ts
@@ -3,11 +3,19 @@ import { apiSlice } from "./api/ApiSlice"
 import { apiSlicenoauth } from "./api/ApiSliceNoAuth"
 import { setupListeners } from "@reduxjs/toolkit/query"
 import authReducer from '../Features/Auth/AuthSlice'
+import carsreducer from '../Features/Car/CarApiSlice'
+//import componentsreducer from from '../Features/Component/ComponentApiSlice'
+//reducers for each table in the database
 export type RootState = {
-    [apiSlice.reducerPath]: ReturnType<typeof apiSlice.reducer>;
-    [apiSlicenoauth.reducerPath]: ReturnType<typeof apiSlicenoauth.reducer>;
-    auth: ReturnType<typeof authReducer>;
+    [apiSlice.reducerPath]: ReturnType<typeof apiSlice.reducer>
+    [apiSlicenoauth.reducerPath]: ReturnType<typeof apiSlicenoauth.reducer>
+    auth: ReturnType<typeof authReducer>
+    cars: ReturnType<typeof carsreducer>
+    //components: ReturnType<typeof componentsreducer>
 };
+//Adding reducer paths
+//apiSlicenoauth is for the apislice that does not need authentiction to send an api request i.e(register)
+//apiSlice requires credentials and a token in its header to allow for api requests
 export const Store = configureStore({
   reducer: { 
     [apiSlicenoauth.reducerPath]: apiSlicenoauth.reducer, 
