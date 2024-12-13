b3f3860143481a0207fb9ca2e331e8aa871a9e4f
zeyadmedhat <zeyad.medhat2000@gmail.com>
2024-12-13 18:07:33 +0200

    added Authentication routes

---
+++ server/src/Routes/AuthRoutes.ts
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const AuthController = require ('../Controllers/AuthController')
+const loginLimiter = require('../middleware/loginLimiter')
+
+router.route('/')
+    .post(loginLimiter,AuthController.login)
+
+router.route('/refresh')
+    .get(AuthController.refresh)
+
+router.route('/logout')
+    .post(AuthController.logout)
+
+module.exports = router
