4c27f9cbdf146ea9ae42267deb78f80e7e6bab26
zeyadmedhat <zeyad.medhat2000@gmail.com>
2024-12-13 18:06:59 +0200

    added verify json web token

---
+++ server/src/Middleware/VerifyJWT.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction} from 'express';
+const jwt = require('jsonwebtoken')
+
+const verifyJWT = (req:Request,res:Response,next:NextFunction) => {
+    const authHeader = req.headers.authorization || req.headers.Authorization
+
+    if(!authHeader?.startsWith('Bearer ')){
+        return res.status(401).json({message:'Unauthorized'})
+    }
+
+    const token = authHeader.split(' ')[1]
+
+    jwt.verify(
+        token,
+        process.env.ACCESS_TOKEN_SECRET,
+        (err, decoded) =>{
+            if(err) return res.status(403).json({ message: 'Forbidden'})
+            req.user = decoded.UserInfo.username
+            req.roles = decoded.UserInfo.roles
+            next() //next middleware or controller
+        }
+        
+    )
+}
+
+module.exports = verifyJWT
