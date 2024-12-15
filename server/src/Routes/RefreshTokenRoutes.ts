bcc50c33f46ddaa6589883604bcd56b9a336b255
zeyadmedhat <zeyad.medhat2000@gmail.com>
2024-12-15 15:27:34 +0200

    Added Refresh token routes

---
+++ server/src/Routes/RefreshTokenRoutes.ts
@@ -0,0 +1,12 @@
+const { issueRefreshToken, validateRefreshToken, revokeRefreshToken } = require('../controllers/refreshTokenController');
+
+// Route to issue a new refresh token
+router.post('/issue', issueRefreshToken);
+
+// Route to validate a refresh token and issue a new access token
+router.post('/validate', validateRefreshToken);
+
+// Route to revoke a refresh token
+router.post('/revoke', revokeRefreshToken);
+
+module.exports = router;
