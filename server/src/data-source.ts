589cf7f5b74d8edcbdb738b7562412d1ed222795
zeyadmedhat <zeyad.medhat2000@gmail.com>
2024-12-15 18:18:11 +0200

    Added Car and Component entities

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -2,6 +2,8 @@ import { DataSource } from 'typeorm';
 import { User } from './Entities/User'; 
 import { RefreshToken } from './Entities/RefreshToken';
 import dotenv from 'dotenv'
+import { Car } from './Entities/Car';
+import { Component } from './Entities/Component';
 
 dotenv.config();
 
@@ -12,7 +14,7 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: [User, RefreshToken],
+  entities: [User, Car, Component, RefreshToken],
   synchronize: true,  // Set to false in production
   logging: true,
 });
