1aeca34041396883fe1fcd907a44fa47f987ca61
zeyadmedhat <zeyad.medhat2000@gmail.com>
2024-12-15 15:46:10 +0200

    added appropriate comments

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -1,18 +1,18 @@
 import { DataSource } from 'typeorm';
-import { User } from './Entities/User';  // Adjust the path accordingly
-import { RefreshToken } from './Entities/RefreshToken'; // Adjust the path accordingly
+import { User } from './Entities/User'; 
+import { RefreshToken } from './Entities/RefreshToken';
 import dotenv from 'dotenv'
 
 dotenv.config();
 
 export const AppDataSource = new DataSource({
-  type: 'postgres',  // Adjust if you're using a different database
+  type: 'postgres',  // database type
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: [User, RefreshToken],  // Add your entities here
-  synchronize: true,  // Set to false in production to avoid automatic schema sync
+  entities: [User, RefreshToken],
+  synchronize: true,  // Set to false in production
   logging: true,
 });
