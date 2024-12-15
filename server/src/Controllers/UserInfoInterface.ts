a907bbbac74a3e6673841ad3b4e52d7d878bd89e
zeyadmedhat <zeyad.medhat2000@gmail.com>
2024-12-15 15:23:35 +0200

    Modified user info to match user's fields

--- server/src/Controllers/UserInfoInterface.ts
+++ server/src/Controllers/UserInfoInterface.ts
@@ -1,5 +1,6 @@
 export interface UserInfo {
-    username: string;
-    roles: string[];
-    id: string;
+    CarUserID: number
+    UserName: string
+    Password: string
+    CarsOwned: number
 }
