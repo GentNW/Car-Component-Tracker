cddd336ff6034128fca9a4726e978da60bc606e6
zeyadmedhat <zeyad.medhat2000@gmail.com>
2024-12-16 23:29:55 +0200

    Added error handling for routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,8 @@ import express from 'express'
 import UserRoutes from './Routes/UserRoutes'
 import ComponentRoutes from './Routes/ComponentRoutes'
 import CarRoutes from './Routes/CarRoutes'
-
+import ErrorHandler from './Middleware/ErrorHandler'
+import path from 'path'
 
 const app = express();
 
@@ -23,3 +24,24 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+//Catching all (error handling mostly)
+app.all('*',(req,res) => {
+
+  //Handling a not found error
+  res.status(404)
+  if(req.accepts('html')) //Checks HTML file's acceptability
+  {
+      res.sendFile(path.join(__dirname, 'views', '404.html'))
+  } 
+  else if(req.accepts('json')) //Checks json file's acceptability
+  {
+      res.json({ message : "404 Not Found"})
+  } 
+  else
+  {
+      res.type('txt').send('404 Not Found')
+  }
+})
+
+app.use(ErrorHandler)
