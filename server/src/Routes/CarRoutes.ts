10ec2d72cc59e719aee9e3bc3f909ae9f904ecfc
zeyadmedhat <zeyad.medhat2000@gmail.com>
2024-12-17 19:57:23 +0200

    Added the new car route

--- server/src/Routes/CarRoutes.ts
+++ server/src/Routes/CarRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
-import {createCar,updateCar,getCars,deleteCar } from '../Controllers/CarController'
+import {createCar,updateCar,getCars,deleteCar, getCar, getCarComponents } from '../Controllers/CarController'
 
 const router = Router();
 
 router.post('/car', createCar);
-router.get('/car', getCars);
+router.get('/cars', getCars);
+router.get('/car/:id', getCar)
+router.get('/car/components',getCarComponents)
 router.put('/car/:id', updateCar);
 router.delete('/car/:id', deleteCar);
 
