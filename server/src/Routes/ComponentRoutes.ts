a022d1569d94f26c5bf89917acb3eabe41c071d9
zeyadmedhat <zeyad.medhat2000@gmail.com>
2024-12-17 19:56:21 +0200

    Added the new component routes

--- server/src/Routes/ComponentRoutes.ts
+++ server/src/Routes/ComponentRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { createComponent, getComponents, updateComponent, deleteComponent } from '../Controllers/ComponentController';
+import { createComponent, getComponents, updateComponent, deleteComponent, getComponentByID } from '../Controllers/ComponentController';
 
 const router = Router();
 
 router.post('/component', createComponent);
 router.get('/component', getComponents);
+router.get('/component/:id', getComponentByID);
 router.put('/component/:id', updateComponent);
 router.delete('/component/:id', deleteComponent);
 
