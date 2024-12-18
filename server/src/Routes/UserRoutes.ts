92a8bf64d88902c1993479e1deb63f48716282fe
zeyadmedhat <zeyad.medhat2000@gmail.com>
2024-12-18 16:18:29 +0200

    Added new routes

--- server/src/Routes/UserRoutes.ts
+++ server/src/Routes/UserRoutes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
-import { createUser, getUsers, updateUser, deleteUser } from '../Controllers/UserController'
+import { createUser, getUsers,getUser, updateUser, deleteUser, getUserCars } from '../Controllers/UserController'
+//const verifyJWT = require('../middleware/verifyJWT')
+
+// router.use(verifyJWT)
 
 const router = Router();
 
 router.post('/user', createUser);
-router.get('/user', getUsers);
+router.get('/users', getUsers);
+router.get('/user/:id', getUser);
+router.get('/user/cars',getUserCars)
 router.put('/user/:id', updateUser);
 router.delete('/user/:id', deleteUser);
 
