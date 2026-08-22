0804eb50ccaa2d1fc008dc3f7ca65a884a136fcf
zeyad M. Abdelrahman <zeyad.medhat2000@gmail.com>
2026-08-22 09:47:31 +0300

    Added initial Config

---
+++ server/src/Config/AllowOrigins.ts
@@ -0,0 +1,6 @@
+// const AllowedOrigins = [
+//     'http://localhost:3500',
+//     'https://www.car-doctor.com',
+//     'https://car-doctor.com',
+// ]
+// export const AllowedOrigin = AllowedOrigins
---
+++ server/src/Config/CorsOptions.ts
@@ -0,0 +1,15 @@
+// import {AllowedOrigin} from './AllowOrigins'
+
+// const corsOptions =  {
+//     origin: (origin, callback) =>{
+//         if(AllowedOrigin.indexOf(origin) !== -1 || !origin)
+//         {
+//             callback(null, true)
+//         }else{
+//             callback(new Error('Not Allowed by CORS'))
+//         }
+//     },
+//     credentials: true,
+//     optionSuccessStatus: 200
+// }
+// export const corsOption = corsOptions 
