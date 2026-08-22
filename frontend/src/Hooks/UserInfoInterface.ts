f37f226cfabfde2c900cc56fa4dbfee44cad5efe
zeyad M. Abdelrahman <zeyad.medhat2000@gmail.com>
2026-08-22 14:14:09 +0300

    Modified Userinfo to match its table

--- frontend/src/Hooks/UserInfoInterface.ts
+++ frontend/src/Hooks/UserInfoInterface.ts
@@ -1,5 +1,7 @@
 export interface UserInfo {
-    username: string;
-    roles: string[];
-    id: string;
+    CarUserID: number
+    UserName: string
+    Password: string
+    CarsOwned: number
+    isLoggedin: boolean
 }
