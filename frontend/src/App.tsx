c6243946d832be4696aec159100eadf5d8931cfa
zeyad M. Abdelrahman <zeyad.medhat2000@gmail.com>
2026-08-22 09:36:35 +0300

    Added Car related routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,9 @@ import Layout from './Components/Layout';
 import NewComponent from './Features/Component/NewComponent';
 import DashLayout from './Components/DashLayout';
 import EditComponent from './Features/Component/EditComponent';
+import Welcome from './Features/Auth/Welcome';
+import NewCar from './Features/Car/NewCar';
+import EditCar from './Features/Car/EditCar';
 function App() {
   return (
     <>
@@ -13,8 +16,11 @@ function App() {
         <Route path='/' element={<Layout/>}>
         <Route index element={<Homepage/>}/>
           <Route path = "dash" element={<DashLayout />}>
+            <Route index element={<Welcome/>}/>
             <Route path='newcomp' element={<NewComponent/>}/>
             <Route path='editcomp' element={<EditComponent/>}/>
+            <Route path='Addcar' element={<NewCar/>}/>
+            <Route path='editcar' element={<EditCar/>}/>
           </Route>{/*end of Dash layout*/}
         </Route>{/*end of layout*/}
       </Routes>
